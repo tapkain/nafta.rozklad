a4edcde8233e0cec23caf3f9ab7672521022ff29
Eugene Velizhenkov <tredasaibot@gmail.com>
2017-10-04 16:21:23 +0300

    Initial commit for iOS

---
+++ NaftaRozklad/NaftaRozklad/LoginScene/LoginTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LoginTableViewCell.swift
+//  NaftaRozklad
+//
+//  Created by Yevhen Velizhenkov on 10/4/17.
+//  Copyright © 2017 Yevhen Velizhenkov. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewCell: UITableViewCell {
+  @IBOutlet weak var groupNameLabel: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
