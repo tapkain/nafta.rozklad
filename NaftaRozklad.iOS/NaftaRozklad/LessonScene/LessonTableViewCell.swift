9eec5d59606469022b4e4b506b1a49408d3e7b07
Eugene Velizhenkov <tredasaibot@gmail.com>
2017-10-10 09:14:00 +0300

    Added search bar

---
+++ NaftaRozklad.iOS/NaftaRozklad/LessonScene/LessonTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LessonTableViewCell.swift
+//  NaftaRozklad
+//
+//  Created by Yevhen Velizhenkov on 10/7/17.
+//  Copyright © 2017 Yevhen Velizhenkov. All rights reserved.
+//
+
+import UIKit
+
+class LessonTableViewCell: UITableViewCell {
+  static let identifier = String(describing: LessonTableViewCell.self)
+}
--- NaftaRozklad.iOS/NaftaRozklad/LoginScene/LoginViewController.swift
+++ NaftaRozklad.iOS/NaftaRozklad/LoginScene/LoginViewController.swift
@@ -9,43 +9,11 @@
 import UIKit
 import RealmSwift
 
-class LoginViewController: UITableViewController {
+class LoginViewController: UITableViewController, UISearchResultsUpdating {
   let realm = try! Realm()
   lazy var groups: Results<UniversityGroup> = { realm.objects(UniversityGroup.self) }()
   let cellIdentifier = LoginTableViewCell.identifier
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    fetchGroups()
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! LoginTableViewCell
-    let group = groups[indexPath.row]
-    cell.groupNameLabel?.text = group.name
-    
-    return cell
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let groupName = groups[indexPath.row].name
-    let alertController = UIAlertController(title: "Do you really want to choose " + groupName, message: nil, preferredStyle: .alert)
-    let cancelAction = UIAlertAction(title: "NO", style: .cancel)
-    let okAction = UIAlertAction(title: "YES", style: .default) { action in
-      //present VC here
-    }
-    alertController.addAction(okAction)
-    alertController.addAction(cancelAction)
-    present(alertController, animated: true)
-  }
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return groups.count
-  }
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
+  var searchController: UISearchController! = nil
 
   func fetchGroups() {
     ApiManager.sharedInstance.getUniversityGroups { groups in
@@ -68,3 +36,73 @@ class LoginViewController: UITableViewController {
   }
 }
 
+// MARK - Lifecycle & Initialization
+extension LoginViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    fetchGroups()
+    initSearchController()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    searchController.dismiss(animated: false)
+  }
+  
+  func initSearchController() {
+    searchController = UISearchController(searchResultsController: nil)
+    searchController.searchResultsUpdater = self
+    searchController.hidesNavigationBarDuringPresentation = false
+    searchController.dimsBackgroundDuringPresentation = false
+  }
+}
+
+// MARK - UITableViewDelegate
+extension LoginViewController {
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! LoginTableViewCell
+    let group = groups[indexPath.row]
+    cell.groupNameLabel?.text = group.name
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let groupName = groups[indexPath.row].name
+    let alertController = UIAlertController(title: "Do you really want to choose " + groupName, message: nil, preferredStyle: .alert)
+    let cancelAction = UIAlertAction(title: "NO", style: .cancel)
+    let okAction = UIAlertAction(title: "YES", style: .default) { action in
+      //present VC here
+    }
+    alertController.addAction(okAction)
+    alertController.addAction(cancelAction)
+    present(alertController, animated: true)
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return groups.count
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+}
+
+// MARK - UISearchResultsUpdating
+extension LoginViewController {
+  func updateSearchResults(for searchController: UISearchController) {
+    if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+      groups = realm.objects(UniversityGroup.self).filter("name CONTAINS[cd] %@", searchText)
+    } else {
+      groups = realm.objects(UniversityGroup.self)
+    }
+    tableView.reloadData()
+  }
+}
+
+// MARK - Actions
+extension LoginViewController {
+  @IBAction func searchButtonPressed(_ sender: Any) {
+    present(searchController, animated: true)
+  }
+}
